a15829e5dbad7e24efa62b9ffb13d84dd5ac7e6b
LizFern <144252056+LizFern@users.noreply.github.com>
2023-09-11 14:49:21 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const CALCULAR = document.getElementById('calcular');
+const ERROR = document.getElementById('error');
+const FLU = document.getElementById('flu');
+const MAN = document.getElementById('man');
+
+CALCULAR.addEventListener('click', () => {
+    const DATO = document.getElementById('peso').value
+    
+
+
+    if (DATO > 0 && DATO <= 30 ){
+        ERROR.style.display = 'none'
+        let flujo = calcFlujo(DATO);
+        let mantenimiento = flujo*1.5;
+        FLU.innerHTML = flujo + ' cc/hr';
+        MAN.innerHTML = 'm+m/2 ' + mantenimiento + ' cc/hr';
+        FLU.style.display = 'block';
+        MAN.style.display = 'block';
+        
+        
+    } else if (DATO > 30 ) {
+
+      ERROR.style.display = 'none'
+      let flujo = ((DATO * 4) + 7 ) / (DATO + 90);
+      FLU.innerHTML = Math.round (flujo * 1500) + ' cc/hr';
+      MAN.innerHTML = Math.round (flujo * 2000) + ' cc/hr';
+      FLU.style.display = 'block';
+      MAN.style.display = 'block';
+    }
+    
+    else {
+        ERROR.style.display = 'block';
+        FLU.style.display = 'none';
+        MAN.style.display = 'none';
+    }
+})
+
+function calcFlujo(peso){
+  let resto = peso;
+  let flujo = 0;
+  if (resto>20){
+      let aux = resto-20;
+      flujo += aux*1;
+      resto -= aux;
+  } 
+  if (resto>10){
+      let aux = resto-10;
+      flujo += aux*2;
+      resto -= aux;
+  }
+  flujo += resto*4;
+  return flujo;
+}
